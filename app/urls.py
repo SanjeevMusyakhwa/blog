c9e051c3e2bedb8a20c448f07f8a159e023192c9
sanjeevmusyakhwa <sanjeevmusyakhwa@gmail.com>
2024-10-07 12:33:22 +0545

    changes made

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from app import views
 urlpatterns = [
-    path('', views.postList, name='postList')
+    path('', views.postList, name='postList'),
+    path('postDetail/<int:pk>/', views.postDetail, name='postDetail'),
+    path('postDelete/<int:pk>/', views.postDelete, name='postDelete'),
+    path('postUpdate/<int:pk>/', views.postUpdate, name='postUpdate'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.models import Post
 # Create your views here.
 def postList(request):
@@ -7,4 +7,31 @@ def postList(request):
     request,
     'postList.html',
     {'posts': posts}
+  )
+
+def postDetail(request, pk):
+  post = Post.objects.get(id = pk)
+  return render(
+    request,
+    'postDetail.html',
+    {'post': post}
+  )
+
+
+def postDelete(request, pk):
+  post = Post.objects.get(id = pk)
+  post.delete()
+  return redirect('postList')
+
+def postUpdate(request, pk):
+  post = Post.objects.get(id = pk)
+  if request.method == 'POST':
+    post.title = request.POST['title']
+    post.content = request.POST['content']
+    post.save()
+    return redirect('postList')
+  return render(
+    request,
+    'postUpdate.html',
+    {'post':post}
   )
